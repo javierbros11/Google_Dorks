6e40d83ea458ec723d744c8a5e999dfc35df4d5b
Javier Limón Polo <javierlimonpolo@gmail.com>
2025-05-11 13:49:38 +0200

    Corrected file google_dork.py

--- Google_Bing_Dork/google_dork.py
+++ Google_Bing_Dork/google_dork.py
@@ -1,10 +1,10 @@
 # External Packages
 
-from dotenv import load_dotenv,set_key
-import os
 import argparse
 import sys
+
 # Local files
+
 from google_config import GoogleConfig
 from browserautosearch_bing import BrowserAutoSearch
 from ai_agent_google import IAAgent
@@ -12,15 +12,6 @@ from google_file_downloader import FileDownloader
 from googlesearch import GoogleSearch
 from results_parser import ResultsParser
 
-# IA Config
-
-def _openai_config(self):
-        """
-        Configura la API_KEY de OpenAI en el fichero .env
-        """
-
-        api_key = input("Introduce tu API_KEY de OpenAI: ")
-        set_key(".env","OPENAI_API_KEY",api_key)
 
 # Main Code
 
@@ -30,6 +21,8 @@ def main(query,configure_env, start_page, pages, lang,output_json,output_html,do
 
     configure = GoogleConfig(configure_env)
     API_KEY_GOOGLE, SEARCH_ENGINE_ID = configure.load_env()
+
+    # Conditions
 
     if gen_dork:
         print("Utilizando gpt4all y ejecutando la generación en local. Puede tardar unos minutos...")
@@ -49,15 +42,15 @@ def main(query,configure_env, start_page, pages, lang,output_json,output_html,do
         browser.quit()
     
     else:
-        #API_KEY_GOOGLE,SEARCH_ENGINE_ID = load_env(configure_env=configure_env)
         busqueda = GoogleSearch(API_KEY_GOOGLE,SEARCH_ENGINE_ID)
         resultados = busqueda.search(query,start_page=start_page,pages = pages,lang=lang)
     
+    # Display the results in command line console
+
     rparser = ResultsParser(resultados)
-
-    # Mostrar los resultados en linea de consola de comandos.
-
     rparser.mostrar_pantalla()
+
+    # Export files
 
     if output_html:
         rparser.exportar_html(output_html)
@@ -65,18 +58,26 @@ def main(query,configure_env, start_page, pages, lang,output_json,output_html,do
     if output_json:
         rparser.exportar_json(output_json)
 
-    # En cuanto a la descarga de ficheros PDF es posibles que algunas descargas fallen debido al uso de CDNs.
+    # Downloader
+
     if download:
-        # Separar las extensiones de los archivos en una lista
+        
         file_types = download.split(",")
-        # Nos quedamos con las URLs de los resultados obtenidos
+        
         urls = [resultado['link'] for resultado in resultados]
         descarga_ficheros = FileDownloader("Descargas_GoogleDorks")
         descarga_ficheros.filtra_descarga(urls,file_types)
 
 if __name__ == "__main__":
-    # Configuración de los argumentos del programa
+
+    # Configuration of program arguments
+
+    # Program description
+
     parser = argparse.ArgumentParser(description = "Esta herramienta permite realizar Hacking con buscadores de forma automática. En este caso emplearemos principalmente el buscador de Google Chrome/Bing.")
+
+    # Program Arguments
+    
     parser.add_argument("-q","--query", type=str,
                         help="Este argumento especifica la consulta a realizar.\nEjemplo: '-q marca.com'")
     parser.add_argument("-c", "--configure", action="store_true",
@@ -98,8 +99,13 @@ if __name__ == "__main__":
                         "Por ejemplo: --generate-dork 'Listado de usuarios y passwds en un fichero de texto.'")
     parser.add_argument("--selenium",action="store_true", default=False,
                         help="Permite realizar una consulta con Selenium, imitando el comportamiento humano a la hora de realizar cualquier búsqueda.")
+    
+    # Definition of arguments
+
     args = parser.parse_args()
     
+    # Calling the main function
+
     main(query = args.query,configure_env=args.configure,start_page=args.start_page,pages=args.pages,
          lang=args.lang,output_json=args.json,output_html=args.html,download=args.download,
          gen_dork = args.generate_dork,selenium = args.selenium)
